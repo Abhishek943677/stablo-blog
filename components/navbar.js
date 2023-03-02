e252171a738c1c3b57c042ee0e6863be0ed367a5
Abhishek943677 <65477844+Abhishek943677@users.noreply.github.com>
2023-03-02 19:28:41 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -49,7 +49,7 @@ export default function Navbar(props) {
           {({ open }) => (
             <>
               <div className="flex flex-wrap justify-between md:gap-10 md:flex-nowrap">
-                <div className="flex-col items-center justify-start order-1 hidden w-full md:flex md:flex-row md:justify-end md:w-auto md:order-none md:flex-1">
+                <div className="flex-col items-center justify-center order-1 hidden w-full md:flex md:flex-row md:justify-end md:w-auto md:order-none md:flex-1">
                   {leftmenu.map((item, index) => (
                     <Link href={item.href} key={index}>
                       <a className="px-5 py-2 text-sm font-medium text-gray-600 dark:text-gray-400 hover:text-blue-500">
